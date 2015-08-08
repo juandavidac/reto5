063cc8ed93fd43f15937fa638e2f2f0f1680195e
juandavidac <jd.ac@outlook.com>
2015-08-07 22:13:52 -0500

    versión1

---
+++ v1.rb
@@ -0,0 +1,38 @@
+puts "Bienvenido a reto 5, para continuar ingresa la traducción correcta"
+puts "Ingresa la traducción de Sensitive"
+ans1=gets.chomp.downcase
+  while ans1!="sensible"
+    puts "inténtalo otra vez"
+    ans1=gets.chomp.downcase
+  end
+puts "Ingresa la traducción de Advice"
+ans2=gets.chomp.downcase
+  while ans2!="consejo"
+    puts "inténtalo otra vez"
+    ans2=gets.chomp.downcase
+  end
+puts "Ingresa la traducción de Misfortune"
+ans3=gets.chomp.downcase
+  while ans3!="desgracia"
+    puts "inténtalo otra vez"
+    ans3=gets.chomp.downcase
+  end
+puts "Ingresa la traducción de Sensible"
+ans4=gets.chomp.downcase
+  while ans4!="sensato"
+    puts "inténtalo otra vez"
+    ans4=gets.chomp.downcase
+  end
+puts "Ingresa la traducción de Advise"
+ans5=gets.chomp.downcase
+  while ans5!="aconsejar"
+    puts "inténtalo otra vez"
+    ans5=gets.chomp.downcase
+  end
+puts "Ingresa la traducción de Disgrace"
+ans6=gets.chomp.downcase
+  while ans6!="deshonra"
+    puts "inténtalo otra vez"
+    ans6=gets.chomp.downcase
+  end
+  
